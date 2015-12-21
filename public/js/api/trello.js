c2a9aa92d39364d1eb783a712fbe99b0792193a1
Joe <bazzooka@gmail.com>
2015-12-21 17:17:59 +0100

    update Trello authentication

--- public/js/api/trello.js
+++ public/js/api/trello.js
@@ -28,9 +28,9 @@ var Trello = {
   },
 
   getScope: function(scope) {
-    var ref1, results, k, v, ref1 = scope, 
+    var ref1, results, k, v, ref1 = scope,
     results = [];
-    
+
     for (k in ref1) {
       v = ref1[k];
       if (v) {
@@ -41,95 +41,85 @@ var Trello = {
     return results.join(",");
   },
 
-  authorize :  function() {
-    let regexToken = /[&#]?token=([0-9a-f]{64})/,
-    ref1 = null,
-      origin = (ref1 = /^[a-z]+:\/\/[^\/]*/.exec(location)) != null ? ref1[0] : void 0,
-		  token = null, opts = {}, scope = "",
-      authorizeURL = "";
-	
-  	Object.assign(opts, {
-  		type: "popup",
-  		persist: true,
-  		interactive: true,
-  		scope: {
-  		  read: true,
-  		  write: true,
-  		  account: false,
-  		},
-  		expiration: "30days",
-  	});
-      
-    if (token === null) {
-      token = this.readStorage("token");
-    }
+  deauthorize: function(){
+    this.writeStorage("token", null);
+  },
 
-  	// if (token == null) {
-   //    token = (ref = regexToken.exec(location.hash)) != null ? ref[1] : void 0;
-   //  }
+  authorize:  function() {
+    return new Promise(function(resolve, reject) {
+      let regexToken = /[&#]?token=([0-9a-f]{64})/,
+      ref1 = null,
+        origin = (ref1 = /^[a-z]+:\/\/[^\/]*/.exec(location)) != null ? ref1[0] : void 0,
+        token = null, opts = {}, scope = "",
+        authorizeURL = "";
 
-      // if (this.authorized()) {
-      //   persistToken();
-      //   location.hash = location.hash.replace(regexToken, "");
-      //   return typeof opts.success === "function" ? opts.success() : void 0;
-      // }
+      window.addEventListener("message", function(event) {
+        var _ref3;
 
-      console.log("Check is authorized");
+        if (event.origin !== this.opts.authEndpoint) {
+          return true;
+        }
 
-      // if (!opts.interactive) {
-      //   return typeof opts.error === "function" ? opts.error() : void 0;
-      // }
+        if ((_ref3 = event.source) != null) {
+          _ref3.close();
+        }
 
-    scope = this.getScope(opts.scope);
+        console.log(event);
+        this.persistToken(event.data);
+        resolve("OK");
+      }.bind(this));
 
-    var authorizeUrl = "https://trello.com" + "/" + 1 + "/authorize?";
-    authorizeUrl += "response_type=token";
-    authorizeUrl += "&key=" + this.opts.key;
-    authorizeUrl +="&return_url=" + origin;
-    authorizeUrl += "&callback_method=postMessage";
-    authorizeUrl += "&success=postMessage";
-    authorizeUrl += "&scope=" +  scope,
-    authorizeUrl += "&expiration=" + "30days";
-    authorizeUrl += "&name=" + "MyCal";
+      Object.assign(opts, {
+        type: "popup",
+        persist: true,
+        interactive: true,
+        scope: {
+          read: true,
+          write: true,
+          account: false,
+        },
+        expiration: "30days",
+      });
 
+      if (token === null) {
+        token = this.readStorage("token");
+      }
 
-    var width = 420,
+      if (token != null) {
+        resolve("OK");
+        return true;
+      }
+
+      scope = this.getScope(opts.scope);
+
+      var authorizeUrl = "https://trello.com" + "/" + 1 + "/authorize?";
+      authorizeUrl += "response_type=token";
+      authorizeUrl += "&key=" + this.opts.key;
+      authorizeUrl += "&return_url=" + origin;
+      authorizeUrl += "&callback_method=postMessage";
+      authorizeUrl += "&success=postMessage";
+      authorizeUrl += "&scope=" +  scope,
+      authorizeUrl += "&expiration=" + "30days";
+      authorizeUrl += "&name=" + "MyCal";
+
+      var width = 420,
         height = 470,
         left = window.screenX + (window.innerWidth - width) / 2,
         top = window.screenY + (window.innerHeight - height) / 2;
 
-      
-    if (opts.type) {
-      (function() {
-        var height, left, origin, ref1, top, width;
-        // waitUntil("authorized", (function(_this) {
-        //   return function(isAuthorized) {
-        //     if (isAuthorized) {
-        //       persistToken();
-        //       return typeof opts.success === "function" ? opts.success() : void 0;
-        //     } else {
-        //       return typeof opts.error === "function" ? opts.error() : void 0;
-        //     }
-        //   };
-        // })(this));
-//https://trello.com/1/authorize?response_type=token&key=00f1dd1e00000f1dd1e00000f1dd1e00&return_url=http%3A%2F%2Ffiddle.jshell.net&callback_method=postMessage&scope=read&expiration=30days
-        width = 420;
-        height = 470;
-        left = window.screenX + (window.innerWidth - width) / 2;
-        top = window.screenY + (window.innerHeight - height) / 2;
-        origin = (ref1 = /^[a-z]+:\/\/[^\/]*/.exec(location)) != null ? ref1[0] : void 0;
-        return window.open(authorizeUrl, "trello", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
-      })();
-    }
-  },
-
-  postMessage: function(){
-    console.log(arguments);
+      if (opts.type) {
+        (function() {
+          var height, left, origin, ref1, top, width;
+          width = 420;
+          height = 470;
+          left = window.screenX + (window.innerWidth - width) / 2;
+          top = window.screenY + (window.innerHeight - height) / 2;
+          origin = (ref1 = /^[a-z]+:\/\/[^\/]*/.exec(location)) != null ? ref1[0] : void 0;
+          return window.open(authorizeUrl, "trello", "width=" + width + ",height=" + height + ",left=" + left + ",top=" + top);
+        })();
+      }
+    }.bind(this));
   }
-
 }
-
-window.API = window.API || {};
-window.API.Trello = window.API.Trello || Trello;
 
 export default Trello;
