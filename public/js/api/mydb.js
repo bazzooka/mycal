c4104c34ef52a52b8ec5edcba3a54d8188a4c816
Joe <bazzooka@gmail.com>
2015-12-23 14:18:37 +0100

    begining of database connector for client

---
+++ public/js/api/mydb.js
@@ -0,0 +1,19 @@
+import request from 'superagent';
+
+const MyDb = {
+	downloadTrellosInDb: (trelloToken, userId) => {
+		return new Promise((resolve, reject) => {
+			request.get
+		})
+	},
+
+	addTrelloDatas: (datas) => {
+
+	},
+
+	getTrelloDatas: () => {
+
+	}
+};
+
+export default MyDb
