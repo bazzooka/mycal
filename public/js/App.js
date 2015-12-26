8837ec0f3b1c8ed1383ace11657773cd0b288791
Joe <bazzooka@gmail.com>
2015-12-26 15:32:00 +0100

    reformat for airbnb lint rules

--- public/js/App.js
+++ public/js/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import styles from '../css/App.css';
+import '../css/App.css';
 
-import {AppBar} from 'material-ui/lib/';
+import { AppBar } from 'material-ui/lib/';
 
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props); 
-    this.state = {test: 'foo'}; 
-  }
+// export default class App extends React.Component {
+const App = React.createClass({
+  propTypes: {
+    children: React.PropTypes.element.isRequired,
+  },
+
+  getInitialState() {
+    return { test: 'foo' };
+  },
+
   render() {
     return (
       <div>
         <AppBar title="Title" iconClassNameRight="muidocs-icon-navigation-expand-more" />
-        {this.props.children}
+        { this.props.children }
       </div>
     );
-  }
-}
+  },
+});
+
+export default App;
